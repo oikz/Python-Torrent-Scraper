7d0160355b46513b5fdc0c31336dffbc29147944
oikz <chapmanjgm@gmail.com>
2019-07-18 18:29:22 +1200

    Restart Function + Listing Sites + Class Testing

---
+++ Class Testing.py
@@ -0,0 +1,17 @@
+#classes
+
+class Scraper:
+    def __init__(self, searchterm, sites):
+        self.search = searchterm
+        self.sites = sites
+
+search1 = Scraper("John", "Thepiratebay")
+
+print (search1.search)
+print (search1.sites)
+
+
+#use the scraper in a class and send in (searchterm, websites to search)
+
+
+#or previous searches???
--- Project.py
+++ Project.py
@@ -1,28 +1,92 @@
 from tkinter import *
+
+def restart():
+
+    menuframe1.pack_forget()
+    menuframe2.pack_forget()
+    menuframe3.pack_forget()
+    searchedframe1.pack_forget()
+    searchedframe2.pack_forget()
+    searchedframe3.pack_forget()
+    searchedframe4.pack_forget()
+    searchbutton()
 def searched():
+    global searchedframe1
+    global searchedframe2
+    global searchedframe3
+    global searchedframe4    
     menuframe1.pack_forget()
     menuframe2.pack_forget()
     menuframe3.pack_forget()
     searchedframe1=Frame(window)
     searchedframe1.pack()
-    title=Label(searchedframe1, text="Results:", font=("Arial Bold", 25))
+    searchedframe2=Frame(window)
+    searchedframe2.pack()
+    searchedframe3=Frame(window)
+    searchedframe3.pack()
+    searchedframe4=Frame(window)
+    searchedframe4.pack()
+    if searchbox.get()=="Must be longer than 3 characters":
+        searchbutton()
+    else:
+        print("")
+    if thepiratebay.get() == 1 or x1337.get() == 1 or rarbg.get() == 1 or limetorrents.get() == 1 or katcr.get() == 1 or torrentdownloads.get() == 1:
+        print("continuing")
+    else:
+        title=Label(searchedframe1, text="Please select at least one website")
+    title=Label(searchedframe1, text="Results for: " + searchbox.get(), font=("Arial Bold", 25))
+    if thepiratebay.get():
+        piratebaylabel=Label(searchedframe2, text="ThePirateBay.org")
+        piratebaylabel.pack(side=LEFT)
+    else:
+        print("")
+    if x1337.get():
+        x1337label=Label(searchedframe2, text="1337x.to")
+        x1337label.pack(side=LEFT)
+    else:
+        print("")
+    if rarbg.get():
+        rarbglabel=Label(searchedframe2, text="rarbg.to")
+        rarbglabel.pack(side=LEFT)
+    else:
+        print("")
+    if limetorrents.get():
+        limetorrentslabel=Label(searchedframe2, text="LimeTorrents")
+        limetorrentslabel.pack(side=LEFT)
+    else:
+        print("")
+    if katcr.get():
+        katcrlabel=Label(searchedframe2, text="Kickass Torrents")
+        katcrlabel.pack(side=LEFT)
+    else:
+        print("")
+    if torrentdownloads.get():
+        torrentdownloadslabel=Label(searchedframe2, text="Torrent Downloads")
+        torrentdownloadslabel.pack(side=LEFT)
     title.pack()
-    print("thepiratebay")
-    print(thepiratebay.get())
-    print("1337x")
-    print(x1337.get())
-    print("rarbg")
-    print(rarbg.get())
-    print("limetorrents")
-    print(limetorrents.get())
-    print("katcr")
-    print(katcr.get())
-    print("torrentdownloads")
-    print(torrentdownloads.get())
+
+    tryagainbutton=Button(searchedframe4, text="New Search", command=restart)
+    tryagainbutton.pack(side=RIGHT)
+    #print("thepiratebay")
+    #print(thepiratebay.get())
+    #print("1337x")
+    #print(x1337.get())
+    #print("rarbg")
+    #print(rarbg.get())
+    #print("limetorrents")
+    #print(limetorrents.get())
+    #print("katcr")
+    #print(katcr.get())
+    #print("torrentdownloads")
+    #print(torrentdownloads.get())
 
     try:
         if len(searchbox.get()) <= 3:
-            print("Too short")
+            #Sends the user back to the search page if their search term is smaller than 3 characters
+            searchedframe1.pack_forget()
+            searchedframe2.pack_forget()
+            tooshort=True
+            searchbutton()
         else:
             print("Nice")
 
@@ -31,32 +95,56 @@ def searched():
 
 def cleartext():
     searchbox.delete(0, END)
+    #Clears the searchbox so the user can try again
+    menuframe1.pack_forget()
+    #Clears the menuframes so they can be remade 
+    menuframe2.pack_forget()
+    menuframe3.pack_forget()
     searchbutton()
 
+def searchboxfocused(event):
+#Function is called when the searchbox is clicked on
+    if searchbox.get() == 'Must be longer than 3 characters':
+       searchbox.delete(0, "end") # delete all the text in the entry
+       searchbox.insert(0, '') #Insert blank for user input
+       searchbox.config(fg = 'black')
+
 def searchbutton():
+    global tooshort
     global window
     global searchbox
     global menuframe1
     global menuframe2
     global menuframe3
     menuframe1=Frame(window)
-    menuframe1.pack_forget()
+    #Creates menuframe1 in the wndow element
     menuframe1.pack()
+    #Packs the menuframe1 into the window created
     menuframe2=Frame(window)
-    menuframe2.pack_forget()
     menuframe2.pack()
     menuframe3=Frame(window)
-    menuframe3.pack_forget()
     menuframe3.pack()
     window.title("Torrent Scraper")
+    #Gives the window a title
     title = Label(menuframe1, text="Welcome to this Program", font=("Arial Bold", 25))
     title.pack(pady=(25,30), side=TOP)
     # A basic label with a title "Welcome to this program" using Arial Bold Font size 25
     # Packs the Label into the menuframe and aligns it TOP with padding above of 25px and padding below of 30
+    if tooshort:
+        title2=Label(menuframe1, text="Your search must be longer than 3 characters", front=("Arial Bold", 20))
+        title2.pack(side=TOP)
+    else:
+        print("")
     searchbox = Entry(menuframe1, text="Enter Search Term", width="35")
     searchbox.pack(side=TOP, pady=10)
     # A basic searchbox for the user to enter their search term to be used by the program to search sites
     # Packs the searchbox into the menuframe, aligned TOP and with a padding of 10px above and below
+    searchbox.delete(0, END)
+    searchbox.insert(0, 'Must be longer than 3 characters')
+    searchbox.config(fg = 'grey')
+    #Adds a background text to the box to tell the user that the search must be longer than 3 characters
+    searchbox.bind('<FocusIn>', searchboxfocused)
+    #Enables the searchbox to show "must be longer than 3 characters" until the user clicks it, then it disappears
     search = Button(menuframe1, text="Search", command=searched)
     search.pack(side=RIGHT)
     # Adds a search button for the user to execute the search with their chosen options and executes the searched() function
@@ -87,7 +175,7 @@ def searchbutton():
 
 
 
-
+#Declare Global Variables
 global search
 global searchbox
 global window
@@ -100,17 +188,22 @@ global torrentdownloads
 global menuframe1
 global menuframe2
 global menuframe3
+global tooshort
 
 
-
+tooshort=False
 window=""
 window=Tk()
 window.geometry('650x250')
+#Set window size to 650x250 pixels
 thepiratebay=IntVar(window)
 x1337=IntVar(window)
 rarbg=IntVar(window)
 limetorrents=IntVar(window)
 katcr=IntVar(window)
 torrentdownloads=IntVar(window)
+#Declare future variables used in widgets
 searchbutton()
+#Calls first function
 window.mainloop()
+#Starts the window loop to keep it open
